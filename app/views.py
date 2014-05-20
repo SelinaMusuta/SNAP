eef5f5692d88abd682c4f59cd3315a2c0c807ab1
Selina Musuta <selinamusuta>
2014-05-20 14:37:27 -0400

    Added Heroku files; configured heroku for deployment

--- app/views.py
+++ app/views.py
@@ -6,3 +6,9 @@ from flask import render_template
 @app.route('/')
 def index():
 	return render_template('SNAP.html')
+
+@app.route('/addresses')
+def addresses():
+	return render_template('addresses.html')
+
+
